189a541cbe55ba4360e0705ea13aa081e81f2744
PowerNinja16 <sander16.schepers@gmail.com>
2026-06-28 18:44:21 +0200

    help

--- Hello_Web.py
+++ Hello_Web.py
@@ -1,39 +1,22 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class RequestHandler(BaseHTTPRequestHandler):
-<<<<<<< HEAD
-    '''Handle HTTP requests by returning a fixed "page".'''
-=======
     '''Handle HTTP requests by returning a dynamic page.'''
->>>>>>> b438d09bbbbcbdc65e0de2fb7608b57bd0b33b13
 
     Page = '''\
 <html>
 <body>
 <p>Hello, web!</p>
-<<<<<<< HEAD
-=======
 <p>Time: {date_time}</p>
 <p>Your IP: {client_host}</p>
 <p>Port: {client_port}</p>
 <p>Request: {command}</p>
 <p>Path: {path}</p>
->>>>>>> b438d09bbbbcbdc65e0de2fb7608b57bd0b33b13
 </body>
 </html>
 '''
 
     def do_GET(self):
-<<<<<<< HEAD
-        content = self.Page.encode("utf-8")
-
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(content)))
-        self.end_headers()
-
-        self.wfile.write(content)
-=======
         page = self.create_page()
         self.send_page(page)
 
@@ -45,7 +28,6 @@ class RequestHandler(BaseHTTPRequestHandler):
             'command': self.command,
             'path': self.path
         }
-        print(self.Page)
         return self.Page.format(**values)
 
     def send_page(self, content):
@@ -57,7 +39,6 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         self.wfile.write(content_bytes)
->>>>>>> b438d09bbbbcbdc65e0de2fb7608b57bd0b33b13
 
 #----------------------------------------------------------------------
 
@@ -65,8 +46,4 @@ if __name__ == '__main__':
     serverAddress = ('', 8080)
     server = HTTPServer(serverAddress, RequestHandler)
     print("Server running on http://localhost:8080")
-<<<<<<< HEAD
     server.serve_forever()
-=======
-    server.serve_forever()
->>>>>>> b438d09bbbbcbdc65e0de2fb7608b57bd0b33b13
