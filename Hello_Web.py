9025580c377f7b6b35188eb2e18ef9097a538333
PowerNinja16 <sander16.schepers@gmail.com>
2026-06-28 21:58:25 +0200

    ServingStaticPages

--- Hello_Web.py
+++ Hello_Web.py
@@ -1,4 +1,5 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
 
 class RequestHandler(BaseHTTPRequestHandler):
     '''Handle HTTP requests by returning a dynamic page.'''
@@ -17,8 +18,33 @@ class RequestHandler(BaseHTTPRequestHandler):
 '''
 
     def do_GET(self):
-        page = self.create_page()
-        self.send_page(page)
+        try:
+            # Figure out what exactly is being requested
+            full_path = os.getcwd() + self.path
+
+            # if it does not exist,
+            if not os.path.exists(full_path):
+                raise Exception("'{0}' not found".format(self.path))
+            
+            # ...it's a file...
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+            
+            # ...it's something we don't handle.
+            else:
+                raise Exception("Unknown object '{0}'".format(self.path))
+            
+        except Exception as msg:
+            self.handle_error(msg)
+
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content.decode("utf-8"))
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
 
     def create_page(self):
         values = {
@@ -39,7 +65,32 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         self.wfile.write(content_bytes)
+    
+        Error_Page = """\
+        <html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+        """
 
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content.encode("utf-8"))
+
+    # Handle unknown objects.
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content, 404)
+
+    # Send actual content.
+    def send_content(self, content, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(content.encode("utf-8"))))
+        self.end_headers()
+        self.wfile.write(content.encode("utf-8"))
 #----------------------------------------------------------------------
 
 if __name__ == '__main__':
